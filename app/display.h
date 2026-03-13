a54dffc83b8e03cc92580007fc15e97e8f317583
Prigent <guillaume@Guillaume-2.local>
2026-03-13 13:43:32 +0100

    écran fonctionnel

---
+++ app/display.h
@@ -0,0 +1,17 @@
+/*
+ * display.h
+ *
+ *  Created on: 11 mars 2026
+ *      Author: guillaume
+ */
+
+#ifndef DISPLAY_H
+#define DISPLAY_H
+
+#include <stdint.h>
+#include <stdbool.h>
+
+void DISPLAY_Init(void);
+void DISPLAY_Test(void);
+
+#endif /* DISPLAY_H */
