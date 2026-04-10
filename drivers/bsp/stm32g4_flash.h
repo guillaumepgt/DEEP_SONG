bfc6967d66b9964322f9779c10267ebd12ceaeea
Basile Morin <basilemorin18@gmail.com>
2026-04-10 15:18:47 +0200

    Audio recorder fonctionnel sur flash (128ms max) attention au main si vous pullez

--- drivers/bsp/stm32g4_flash.h
+++ drivers/bsp/stm32g4_flash.h
@@ -4,6 +4,7 @@
  * @author	vchav
  * @date	Jun 11, 2024
  * @brief	Adaptation du module créé par Samuel Poiraud pour la stm32f103rbt6
+ * Rajout 2 fonctions Basile Morin
  *******************************************************************************
  */
 #ifndef BSP_STM32G4_FLASH_H_
@@ -15,5 +16,8 @@ uint64_t BSP_FLASH_read_doubleword(uint32_t index);
 void BSP_FLASH_set_doubleword(uint32_t index, uint64_t data);
 void BSP_FLASH_dump(void);
 void FLASH_demo(void);
+
+void BSP_FLASH_erase_page(void);
+void BSP_FLASH_write_doubleword_fast(uint32_t index, uint64_t data);
 
 #endif /* BSP_STM32G4_FLASH_H_ */
